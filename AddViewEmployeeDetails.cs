6aebf4b67a9f27b6823efe24a1f34a0879ce57f5
gourab19964u <gourab19964u@gmail.com>
2018-10-09 20:28:14 +0530

    Add View class to get details and add new employee

---
+++ AddViewEmployeeDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Management_System
+{
+    class AddViewEmployeeDetails
+    {
+        public void NewEmployee(ref List<EmployeeDetails> list)
+        {
+            EmployeeDetails EmpAdd = new EmployeeDetails();
+            EmpAdd.CreateEmployee();
+            list.Add(EmpAdd);
+        }
+    }
+}
