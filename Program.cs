324b272238cccb2bd80d410d34a93fe68340a1f0
gourab19964u <gourab19964u@gmail.com>
2018-10-10 12:16:35 +0530

    New features : Menu additon

--- Program.cs
+++ Program.cs
@@ -10,9 +10,10 @@ namespace Employee_Management_System
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            List<EmployeeDetails> list = new List<EmployeeDetails>();
+            List<EmployeeDetails> EmployeeList = new List<EmployeeDetails>();
             int option;
             while(true)
             {
@@ -21,41 +22,21 @@ namespace Employee_Management_System
                 Console.WriteLine("**************************************");
                 Console.WriteLine("OPTION #1: ADD EMPLOYEE DETAILS");
                 Console.WriteLine("OPTION #2: VIEW EMPLOYEE DETAILS");
-                Console.WriteLine("OPTION #3: CHANGE MENU STYLE");
-                Console.WriteLine("OPTION #3: CHANGE MENU STYLE");
+                Console.WriteLine("OPTION #3: CHANGE SCREEN SETTINGS");
+                Console.WriteLine("OPTION #4: EXIT");
                 option = Convert.ToInt32(Console.ReadLine());
                 switch(option)
                 {
                     case 1:
                         AddViewEmployeeDetails AddEmp = new AddViewEmployeeDetails();
-                        AddEmp.NewEmployee(ref list);
+                        AddEmp.NewEmployee(ref EmployeeList);
                         break;
                     case 2:
-                        EmployeeDetails searchEmp = new EmployeeDetails();
-                        Console.WriteLine("Please Enter Employee ID whose details you want to see");
-                        string empID = searchEmp.searchEmployee(Console.ReadLine());
-                        if (list.Capacity >= 1)
-                        {
-                            int EmployeePresent = 0; 
-                            foreach (var item in list)
-                            {
-                                if (empID == item.MatchEmployee())
-                                {
-                                    item.showEmployeeDetails();
-                                    EmployeePresent = 1;
-                                }
-                            }
-                            if( EmployeePresent == 0)
-                            {
-                                Console.WriteLine("Employee does not exist");
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine("No employee exists");
-                        }
+                        AddViewEmployeeDetails seachEmp = new AddViewEmployeeDetails();
+                        seachEmp.SearchEmp(ref EmployeeList);
                         break;
                     case 3:
+                        EmployeeDetails.ColorSet();
                         break;
                     case 4:
                         Environment.Exit(0);
@@ -64,7 +45,6 @@ namespace Employee_Management_System
                 }
                 
             }
-            Console.ReadKey();
         }
     }
 }
