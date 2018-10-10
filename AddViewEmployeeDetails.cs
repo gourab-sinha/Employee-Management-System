e0f5041f06eb6c2d80c227008fbbb35fed5440aa
gourab19964u <gourab19964u@gmail.com>
2018-10-10 12:14:57 +0530

    New features : Color, Menu, Validation

--- AddViewEmployeeDetails.cs
+++ AddViewEmployeeDetails.cs
@@ -14,5 +14,35 @@ namespace Employee_Management_System
             EmpAdd.CreateEmployee();
             list.Add(EmpAdd);
         }
+        public void SearchEmp(ref List<EmployeeDetails> EmployeeList)
+        {
+            EmployeeDetails searchEmp = new EmployeeDetails();
+            Console.WriteLine("Please Enter Employee ID whose details you want to see");
+            string empID = searchEmp.searchEmployee(Console.ReadLine());
+            if (EmployeeList.Capacity >= 1)
+            {
+                int EmployeePresent = 0;
+                foreach (var item in EmployeeList)
+                {
+                    if (empID == item.MatchEmployee())
+                    {
+                        item.showEmployeeDetails();
+                        EmployeePresent = 1;
+                    }
+                }
+                if (EmployeePresent == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Employee does not exist");
+                    Console.ResetColor();
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("No employee exists");
+                Console.ResetColor();
+            }
+        }
     }
 }
